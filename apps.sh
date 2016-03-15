75c9b0761528ea1532c884e4784394353a315e12
Bridget Almas <balmas@gmail.com>
2016-03-15 09:49:27 -0400

    use master of both Nemo and Nautilus

--- apps.sh
+++ apps.sh
@@ -3,7 +3,6 @@ echo "Installing Apps"
 git clone https://github.com/Capitains/flask-capitains-nemo
 git clone https://github.com/Capitains/Nautilus
 cd flask-capitains-nemo
-git checkout 0.0.2dev
 virtualenv -p python3 venv
 source venv/bin/activate
 python setup.py develop
