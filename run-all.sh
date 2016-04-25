828e15794dade6facb0b6f7f48991251ff567eab
Bridget Almas <balmas@gmail.com>
2016-04-25 22:41:05 +0000

    update nemo command line

--- run-all.sh
+++ run-all.sh
@@ -4,7 +4,7 @@ sudo pkill -f capitains-nautilus
 
 cd ~/flask-capitains-nemo
 source venv/bin/activate
-nohup capitains-nemo --host 0.0.0.0 --port 8100 --debug --css /home/vagrant/flask-capitains-nemo/examples/tei.pb.min.css http://localhost:5000 >nemo.log 2>&1 &
+nohup capitains-nemo --host 0.0.0.0 --port 8100 --debug --css="/home/vagrant/flask-capitains-nemo/examples/tei.pb.min.css" http://localhost:5000 >nemo.log 2>&1 &
 
 cd ~/Nautilus
 source venv/bin/activate
--- run-nemo.sh
+++ run-nemo.sh
@@ -2,5 +2,5 @@
 sudo pkill -f capitains-nemo
 cd ~/flask-capitains-nemo
 source venv/bin/activate
-nohup capitains-nemo --debug --host 0.0.0.0 --port 8100 --css /home/vagrant/flask-capitains-nemo/examples/tei.pb.min.css http://localhost:5000 >nemo.log 2>&1 &
+nohup capitains-nemo --debug --host 0.0.0.0 --port 8100 --css="/home/vagrant/flask-capitains-nemo/examples/tei.pb.min.css" http://localhost:5000 >nemo.log 2>&1 &
 
